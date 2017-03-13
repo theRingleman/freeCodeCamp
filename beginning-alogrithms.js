8e45977a6a04eeb774ce228878a5361edea50882
theRingleman <sam.ringleman@gmail.com>
2017-03-12 22:09:32 -0600

    add stuff because I guess I dont know how to save things

--- beginning-alogrithms.js
+++ beginning-alogrithms.js
@@ -62,7 +62,7 @@ function mutation(arr) {
   var target = arr[0].toLowerCase();
 
   for (var i = 0; i < test.length; i++) {
-    if (target.indexOf(test[i] > 0)) {
+    if (target.indexOf(test[i]) < 0) {
       return false;
     }
   }
